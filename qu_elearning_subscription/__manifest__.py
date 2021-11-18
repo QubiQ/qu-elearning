0ce969d1fbc31fd29c03ea1b5a27702e19703cdb
Jesus Ramoneda <jesus.ramoneda@qubiq.es>
2021-11-18 09:20:38 +0100

    [ADD] qu_elearning_subscription: Migration to 14

--- qu_elearning_subscription/__manifest__.py
+++ qu_elearning_subscription/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'E-Learning Subscriptions',
     'summary': 'E-Learning Subscriptions',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Knowledge',
     'author': 'QubiQ',
     'website': 'https://www.qubiq.es',
